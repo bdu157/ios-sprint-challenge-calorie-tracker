5a53783e482ab093167dafd5b5690e01d3d34000
Dongwoo Pae <dongwoopae@gmail.com>
2019-09-14 14:15:45 +0900

    hardCoding works for the chart but will need to figure out how the input would change the chart and coreData

--- calorieTracker/SwiftChartViewController.swift
+++ calorieTracker/SwiftChartViewController.swift
@@ -10,25 +10,27 @@ import UIKit
 import SwiftChart
 
 class SwiftChartViewController: UIViewController {
-
+    
     var intake: Int? {
         didSet {
-            self.updateViews()
+            self.makeDatas()
         }
     }
     
-    var datas: [Int] = []
+    let chart = Chart(frame: CGRect(x: 0, y: 0, width: 380, height: 200))
+    
+    let array: [Double] = [300, 400, 600, 100, 600, 100, 200]
+    
+    let series = ChartSeries([300, 400, 600, 100, 600, 100, 200, 100, 200, 500])
+    
+    //var series: ChartSeries
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let chart = Chart(frame: CGRect(x: 0, y: 0, width: 380, height: 200))
         view.addSubview(chart)
         chart.backgroundColor = .cyan
-        let series = ChartSeries([100, 200, 300, 400, 300, 400, 400, 200])
         chart.add(series)
-        
-        observeShouldShowNewIntake()
+        //observeShouldShowNewIntake()
     }
     
     
@@ -37,22 +39,28 @@ class SwiftChartViewController: UIViewController {
     }
     
     @objc func refreshViews(notification: Notification) {
-        view.reloadInputViews()
+//        self.chart.add(self.series)
     }
-
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
+    
+    private func makeDatas() {
 
+        guard let intake = intake else {return}
+
+    }
+    
+    
     private func updateViews() {
         //update chart
         guard let intake = intake else {return}
         
-        
     }
-
+    
 }
